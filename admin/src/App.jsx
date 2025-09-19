52747454350a3f27bd48f6c8e255d020770ad97b
radhika-94 <bradhika237@gmail.com>
2025-09-19 15:34:37 +0530

    k

---
+++ admin/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add.jsx";
+import List from "./pages/List.jsx";
+import Orders from "./pages/Orders.jsx";
+import Login from "./components/Login.jsx";  
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+const App = () => {
+  const [token, setToken] = useState("");
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer/>
+      {token === "" ? (
+        <Login setToken={setToken}/>
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,250px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
